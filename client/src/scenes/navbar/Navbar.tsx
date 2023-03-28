563b2b1407bbf1e575c9df025d6248fffa4a04ff
Thabish <kadertabish2@gmail.com>
2023-03-28 09:44:55 +0400

    navbar
    Signed-off-by: Thabish <kadertabish2@gmail.com>

---
+++ client/src/scenes/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+	return <div>Navbar</div>;
+};
+
+export default Navbar;
