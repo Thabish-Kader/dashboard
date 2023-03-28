3a14a648b5fee9eb64a96538f08b4852e1be2892
Thabish <kadertabish2@gmail.com>
2023-03-28 10:49:26 +0400

    clinet
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 import { themeSettings } from "./theme";
 import { Box, CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./scenes/navbar/navbar";
+import Navbar from "./scenes/navbar/Navbar";
 
 function App() {
 	const theme = useMemo(() => createTheme(themeSettings), []);
